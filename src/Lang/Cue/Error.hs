785758130684071ffb54af8ba382ff710c884d6e
Antoine Leblanc <crucuny@gmail.com>
2021-11-19 15:47:33 +0000

    Introduce new error primitives.

---
+++ src/Lang/Cue/Error.hs
@@ -0,0 +1,22 @@
+module Lang.Cue.Error where
+
+
+--------------------------------------------------------------------------------
+-- Panic
+
+data Panic
+  = UnreachableCode
+  | DemoteBaseValue
+
+unreachable :: a
+unreachable = panic UnreachableCode
+
+panic :: Panic -> a
+panic = error . pp . \case
+  UnreachableCode -> "01: reached supposedly unreachable code"
+  DemoteBaseValue -> "02: attempted to demote an already-demoted value"
+  where
+    pp m = "PANIC\n\
+           \HINT as encountered an unrecoverable internal error.\n\
+           \Please report this error with the following error code and call stack:\n\
+           \ErrorCode: " ++ m
