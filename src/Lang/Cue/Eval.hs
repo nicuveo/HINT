6d137b6641477640641eee0d504b4b107f1afcfd
Antoine Leblanc <crucuny@gmail.com>
2021-12-02 16:12:39 +0000

    More eval stuff.

--- src/Lang/Cue/Eval.hs
+++ src/Lang/Cue/Eval.hs
@@ -13,27 +13,29 @@ import           Lang.Cue.Operation
 -- Evaluate an expression
 
 eval :: Expression -> BaseValue
-eval = resolve . eval'
+eval = resolve . evalExpression
+
+evalExpression :: Expression -> Value
+evalExpression = \case
+  Unary          u -> evalUnary u
+  Multiplication l r v -> run l r v evalMultiplication
+  Division       l r v -> run l r v evalDivision
+  Addition       l r v -> run l r v evalAddition
+  Subtraction    l r v -> run l r v evalSubtraction
+  Equal          l r v -> run l r v evalEqual
+  NotEqual       l r v -> run l r v evalNotEqual
+  Match          l r v -> run l r v undefined -- evalMatch
+  NotMatch       l r v -> run l r v undefined -- evalNotMatch
+  LessThan       l r v -> run l r v evalLessThan
+  LessOrEqual    l r v -> run l r v evalLessOrEqual
+  GreaterThan    l r v -> run l r v evalGreaterThan
+  GreaterOrEqual l r v -> run l r v evalGreaterOrEqual
+  LogicalAnd     l r v -> run l r v undefined -- evalLogicalAnd
+  LogicalOr      l r v -> run l r v undefined -- evalLogicalOr
+  Unification    l r v -> run l r v evalUnification
+  Disjunction    l r v -> run l r v evalDisjunction
   where
-    eval' = \case
-      Unary          u -> evalUnary u
-      Multiplication l r v -> run l r v evalMultiplication
-      Division       l r v -> run l r v evalDivision
-      Addition       l r v -> run l r v evalAddition
-      Subtraction    l r v -> run l r v evalSubtraction
-      Equal          l r v -> run l r v evalEqual
-      NotEqual       l r v -> run l r v evalNotEqual
-      Match          l r v -> run l r v undefined -- evalMatch
-      NotMatch       l r v -> run l r v undefined -- evalNotMatch
-      LessThan       l r v -> run l r v evalLessThan
-      LessOrEqual    l r v -> run l r v evalLessOrEqual
-      GreaterThan    l r v -> run l r v evalGreaterThan
-      GreaterOrEqual l r v -> run l r v evalGreaterOrEqual
-      LogicalAnd     l r v -> run l r v undefined -- evalLogicalAnd
-      LogicalOr      l r v -> run l r v undefined -- evalLogicalOr
-      Unification    l r v -> run l r v evalUnification
-      Disjunction    l r v -> run l r v evalDisjunction
-    run l r v f = foldl1 f $ map eval' $ l:r:v
+    run l r v f = foldl1 f $ map evalExpression $ l:r:v
 
 evalUnary :: UnaryExpression -> Value
 evalUnary (UnaryExpression operators primaryExpression) =
@@ -47,11 +49,27 @@ evalPrimary = \case
   PrimaryIndex    _ _ -> undefined
   PrimarySlice    _ _ -> undefined
 
-evalOperand :: a
-evalOperand = undefined
+evalOperand :: Operand -> Value
+evalOperand = \case
+  OperandName _       -> undefined
+  OperandExpression e -> evalExpression e
+  OperandLiteral l    -> case l of
+    IntegerLiteral i -> Atom $ IntegerAtom i
+    FloatLiteral   f -> Atom $ FloatAtom f
+    StringLiteral  t -> case t of
+      Left  _ -> undefined
+      Right s -> Atom $ StringAtom s
+    BoolLiteral    b -> Atom $ BooleanAtom b
+    NullLiteral      -> Null
+    BottomLiteral    -> Bottom ArisedFromLiteral
+    _                -> undefined
 
 evalUnaryOperator :: Operator -> Value -> Value
 evalUnaryOperator = undefined
 
 resolve :: Value -> BaseValue
-resolve = undefined
+resolve = \case
+  WithDefault v d -> case d of
+    Bottom _ -> v
+    _        -> d
+  v -> demote v
